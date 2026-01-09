86ee8d37400a400ef93e7df3c89d876ee8fe25c4
jisantamariar1 <jisantamariar1@uce.edu.ec>
2026-01-08 19:07:37 -0500

    manejo de eventos con emit

--- src/views/PokemonView.vue
+++ src/views/PokemonView.vue
@@ -1,25 +1,58 @@
 <template>
   <div>
-    <PokemonImagen :pokemonId="1"/>
-    <PokemonOpciones />
+    <PokemonImagen :pokemonId="pokemonGanador" />
+    <PokemonOpciones @seleccionado="evaluarGanador($event)" :listaPokemons="pokemonArr" />
   </div>
+
 </template>
 
 <script>
-    import PokemonImagen from '@/components/PokemonImagen.vue';
-    import PokemonOpciones from '@/components/PokemonOpciones.vue';
+import PokemonImagen from '@/components/PokemonImagen.vue';
+import PokemonOpciones from '@/components/PokemonOpciones.vue';
+import { obtenerVectorPokemonFacade } from '@/clients/PokemonClient.js';
+import { obtenerAleatorioFachada } from '@/clients/PokemonClient.js';
 export default {
-    components: {
-        PokemonImagen,
-        PokemonOpciones
-    },
-    methods: {
-
+  data() {
+    return {
+      pokemonArr: [],
+      pokemonGanador: null,
     }
+  },
+  mounted() {
+    console.log("componente PokemonView montado");
+    this.iniciarJuego();
 
-}
+
+  },
+  components: {
+    PokemonImagen,
+    PokemonOpciones
+  },
+  methods: {
+    async iniciarJuego() {
+      this.pokemonArr = await obtenerVectorPokemonFacade();
+
+      const idAleatorio = obtenerAleatorioFachada(0, 3);
+      this.pokemonGanador = this.pokemonArr[idAleatorio].id;
+
+
+
+    },
+    evaluarGanador(idGanador) {
+      console.log('valor recibido desde padre')
+      console.log(idGanador);
+      if (idGanador === this.pokemonGanador) {
+        console.log('¡Has ganado!');
+      } else {
+        console.log('¡Has perdido! Inténtalo de nuevo.');
+      }
+
+    },
+
+  },
+
+
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
